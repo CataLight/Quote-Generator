4101f592b50c5f11ce08606247e864075e915b21
DimWitRM <DimWitRM@gmail.com>
2019-09-08 19:57:34 -0400

    Added interval for quotes, comments

--- js/script.js
+++ js/script.js
@@ -10,34 +10,59 @@ project 1 - A Random Quote Generator
 var quotes = [
 
   {
-    quote: '“Get busy living or get busy dying.”',
+    quote: 'Get busy living or get busy dying.',
     citation: 'shmoop',
     source: 'Stephen King',
-    year: 1994
+    year: 1994,
+    category: 'life'
   },
   {
-    quote: '“The first step toward success is taken when you refuse to be a captive of the environment in which you first find yourself.”',
-    citation: 'AFB',
+    quote: 'The first step toward success is taken when you refuse to be a captive of the environment in which you first find yourself.',
     source: 'Helen Keller',
-    year: 1929
+    category: 'success'
   },
   {
-    quote: '“Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do."',
+    quote: 'Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do.',
     citation: 'huffpost',
     source: 'Mark Twain',
-    year: 2013
+    year: 2013,
+    category: 'success'
   },
   {
-    quote: '“When I dare to be powerful – to use my strength in the service of my vision, then it becomes less and less important whether I am afraid.”',
+    quote: 'When I dare to be powerful – to use my strength in the service of my vision, then it becomes less and less important whether I am afraid.',
     citation: 'The Cancer Journals',
     source: 'Audre Lorde',
-    year: 1997
+    category: 'life'
   },
   {
-    quote: '“Great minds discuss ideas; average minds discuss events; small minds discuss people.”',
+    quote: 'Great minds discuss ideas; average minds discuss events; small minds discuss people.',
     citation: 'brainyquote',
-    source: 'Eleanor Roosevelt'
-  }
+    source: 'Eleanor Roosevelt',
+    category: 'life'
+  },
+  {  
+    quote: 'I\'m selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can\'t handle me at my worst, then you sure as hell don\'t deserve me at my best.',
+    source: 'Marilyn Monroe',
+    category: 'love'
+  },
+  {  
+    quote: 'You know you\'re in love when you can\'t fall asleep because reality is finally better than your dreams.',
+    source: 'Marilyn Monroe',
+    year: 2013,
+    category: 'love'
+  },
+  {
+    quote: 'Today you are You, that is truer than true. There is no one alive who is Youer than You.',
+    citation: 'odyssey',
+    source: 'Dr.Seuss',
+    category: 'comedy'
+  },
+  {
+    quote: 'Life is a tragedy when seen in close-up, but a comedy in long-shot.',
+    source: 'Charlie Chaplin',
+    year: 1972,
+    category: 'comedy'
+  },
 
 ];
 
@@ -45,9 +70,7 @@ var quotes = [
 
 function getRandomQuote(){
 
-  var rand;
-
-  return rand = Math.floor(Math.random()*quotes.length);
+  return Math.floor(Math.random()*quotes.length);
   
 }
 
@@ -59,14 +82,42 @@ function printQuote(){
 
   htmlQuote += '<p class="quote">' + quotes[randQuote].quote +' </p>'
   htmlQuote += '<p class="source">' + quotes[randQuote].source;
-  htmlQuote += '<span class="citation">' + quotes[randQuote].citation + '</span>';
-  htmlQuote += ' <span class="year"> ' +  quotes[randQuote].year + '</span>';
 
+  // Check to see if there is value on citation and year, if no value then dont show on HTML
+  if(quotes[randQuote].citation != null){
+    htmlQuote += '<span class="citation">' + quotes[randQuote].citation + '</span>';
+  }
+  if(quotes[randQuote].year != null){
+    htmlQuote += ' <span class="year"> ' +  quotes[randQuote].year + '</span>';
+  }
+  if(quotes[randQuote].category != null){
+    htmlQuote += ' <span class="category"> ' +  quotes[randQuote].category + '</span>';
+  }
 
+  htmlQuote += '</p>';
 
   document.getElementById('quote-box').innerHTML = htmlQuote;
 }
 
+
+// function colorChange(){
+
+//   var rand = Math.floor(Math.random()*2);
+//   var htmlQuote = "";
+
+//   if(rand = 1){
+//     htmlQuote += '<p class="quote">';
+//   }
+//   else if(rand = 2){
+//     htmlQuote += '<p class="quote">' + quotes[randQuote].quote +' </p>'
+//   }
+
+// }
+
+
+// Runs the printQuotes function every 5 second to change the quotes
+setInterval(printQuote, 5000);
+// setInterval(colorChange, 5000);
 
 // Calling the printQuote function to change the quote when show another quote button is pressed
 
